d368436a4928157bced141bd6920f88a8918ed6f
crystallcx <crystaltienlee@gmail.com>
2023-03-16 21:46:56 +1000

    add reminder plugin test

---
+++ extensions/reminder.py
@@ -0,0 +1,42 @@
+import hikari
+import lightbulb
+import datetime
+
+plugin = lightbulb.Plugin('Reminders')
+
+def load(bot):
+    bot.add_plugin(plugin)
+
+@plugin.command
+@lightbulb.command('Reminders', 'Reminder Group')
+@lightbulb.implements(lightbulb.SlashCommandGroup)
+async def reminders(ctx):
+    pass #do nothing
+
+# subcommand of my_group
+@reminders.child
+@lightbulb.command('show', 'Show reminders')
+@lightbulb.implements(lightbulb.SlashSubCommand)
+async def subcommand(ctx):
+    await ctx.respond('I am a subcommand!')
+    embed = hikari.Embed(
+        title="Reminders!",
+        description=message,
+    )
+    await ctx.app.rest.create_message(
+        channel=channel.id,
+        content=ping.mention if ping else hikari.UNDEFINED,
+        embed=embed,
+        role_mentions=True,
+    )
+    await ctx.respond(
+        f"Announcement posted to <#{channel.id}>!", flags=hikari.MessageFlag.EPHEMERAL
+    )
+
+# subcommand of my_group
+@reminders.child
+@lightbulb.option('reminder', 'e.g. +$40 shoes', type = str)
+@lightbulb.command('add', 'Add reminder to list')
+@lightbulb.implements(lightbulb.SlashSubCommand)
+async def subcommand(ctx):
+    await ctx.respond(ctx.options.reminder)
